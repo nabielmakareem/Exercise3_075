a173049af55f4174b3820421554af1f1e4ec00b6
nabielmakareem <nabielmakareem10@gmail.com>
2022-11-25 16:26:10 +0700

    add public search

--- Exercise3_075/Program.cs
+++ Exercise3_075/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,20 @@ namespace Exercise_Linked_List_A
         public CircularList()
         {
             LAST = null;
+        }
+        
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+           /*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*if the node is present at the end*/
+                return true;
+            else 
+                return (false);/*return false if the node is not found*/
         }
     }
 }
